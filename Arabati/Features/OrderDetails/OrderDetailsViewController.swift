c9af2d717284212ae91ee081b9f57d2641feb481
Sara Alsunaidi <68998849+sara-sunaidi@users.noreply.github.com>
2022-12-05 18:48:15 +0300

    init view

---
+++ Arabati/Features/OrderDetails/OrderDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OrderDetailsViewController.swift
+//  Arabati
+//
+//  Created by Sara Alsunaidi on 05/12/2022.
+//
+
+import UIKit
+
+class OrderDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
